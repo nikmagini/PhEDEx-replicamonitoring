5459536d361cba77dfcbcbac06322039322be0f3
Aurimas Repecka <arepecka@p05153074636644.cern.ch>
2016-08-15 16:06:15 +0200

    Indentation fix

--- src/scripts/pbr_base.sh
+++ src/scripts/pbr_base.sh
@@ -3,20 +3,20 @@
 # A wrapper script to submit spark job with pbr.sh script
 
 bash pbr.sh --yarn \
-			--basedir hdfs:///project/awg/cms/phedex/block-replicas-snapshots/csv/ \
-			--fromdate 2016-07-01 \
-			--todate 2016-07-07 \
-			--results br_node_bytes \
-			--aggregations delta \
-			--interval 1 \
-			--filt node_name:T2_US_Florida \
-			--fout hdfs:///user/arepecka/ReplicaMonitoring
-			#--keys now \
-			#--order now,node_name \
-			#--asc 0,1 \
-			#--header \
-			#--verbose \
-			#--fname /home/aurimas/CERN/ReplicaMonitoring/v2/data/project/awg/cms/phedex/block-replicas-snapshots/csv/time=2016-07-09_03h07m28s 
+            --basedir hdfs:///project/awg/cms/phedex/block-replicas-snapshots/csv/ \
+            --fromdate 2016-07-01 \
+            --todate 2016-07-07 \
+            --results br_node_bytes \
+            --aggregations delta \
+            --interval 1 \
+            --filt node_name:T2_US_Florida \
+            --fout hdfs:///user/arepecka/ReplicaMonitoring
+            #--keys now \
+            #--order now,node_name \
+            #--asc 0,1 \
+            #--header \
+            #--verbose \
+            #--fname /home/aurimas/CERN/ReplicaMonitoring/v2/data/project/awg/cms/phedex/block-replicas-snapshots/csv/time=2016-07-09_03h07m28s 
 
 
 
